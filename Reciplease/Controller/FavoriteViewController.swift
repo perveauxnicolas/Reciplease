f2b27b91fd91caa14b39d51cbf09fc284872ad43
Nicolas Perveaux <perveauxnicolas@gmail.com>
2024-02-21 11:44:37 +0100

    21 fevrier 2024
    begin

---
+++ Reciplease/Controller/FavoriteViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FavoritesViewController.swift
+//  Reciplease
+//
+//  Created by Perveaux Nicolas on 20/02/2024.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
